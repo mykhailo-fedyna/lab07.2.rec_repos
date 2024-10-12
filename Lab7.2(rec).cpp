2f938c5fc079cb8cd34d1e3324ce42fcf0c69a76
misha-fedyna <mihajlofedina5@gmail.com>
2024-10-12 14:10:49 +0300

    first commit

---
+++ Lab7.2(rec).cpp
@@ -0,0 +1,85 @@
+﻿// Lab_7_2_1.cpp
+// Лабораторна робота № 7.2.2
+// Опрацювання багатовимірних масивів ітераційними способами.
+// Варіант 17
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+void CreateRow(int** a, const int rowNo, const int N, const int Low, const int High, int colNo);
+void CreateRows(int** a, const int K, const int N, const int Low, const int High, int rowNo);
+void PrintRow(int** a, const int rowNo, const int N, int colNo);
+void PrintRows(int** a, const int K, const int N, int rowNo);
+int SearchMaxInRowRecursive(int* row, const int N, int colNo, int maxIndex);
+int SearchMinInRowRecursive(int* row, const int N, int colNo, int minIndex);
+void SwapMaxMinRows(int** a, const int K, const int N, int rowNo);
+int main() {
+    srand((unsigned)time(NULL));
+    int Low, High, N, K;
+    cout << "K = "; cin >> K;
+    cout << "N = "; cin >> N;
+    cout << "Low = "; cin >> Low;
+    cout << "High = "; cin >> High;
+    int** a = new int* [K];
+    for (int i = 0; i < K; i++)
+        a[i] = new int[N];
+    CreateRows(a, K, N, Low, High, 0);
+    PrintRows(a, K, N, 0);
+    SwapMaxMinRows(a, K, N, 1);
+    cout << "Modified Matrix:" << endl;
+    PrintRows(a, K, N, 0);
+    for (int i = 0; i < K; i++)
+        delete[] a[i];
+    delete[] a;
+    return 0;
+}
+void CreateRow(int** a, const int rowNo, const int N, const int Low, const int High, int colNo) {
+    a[rowNo][colNo] = Low + rand() % (High - Low + 1);
+    if (colNo < N - 1)
+        CreateRow(a, rowNo, N, Low, High, colNo + 1);
+}
+void CreateRows(int** a, const int K, const int N, const int Low, const int High, int rowNo) {
+    CreateRow(a, rowNo, N, Low, High, 0);
+    if (rowNo < K - 1)
+        CreateRows(a, K, N, Low, High, rowNo + 1);
+}
+void PrintRow(int** a, const int rowNo, const int N, int colNo) {
+    cout << setw(4) << a[rowNo][colNo];
+    if (colNo < N - 1)
+        PrintRow(a, rowNo, N, colNo + 1);
+    else
+        cout << endl;
+}
+void PrintRows(int** a, const int K, const int N, int rowNo) {
+    PrintRow(a, rowNo, N, 0);
+    if (rowNo < K - 1)
+        PrintRows(a, K, N, rowNo + 1);
+    else
+        cout << endl;
+}
+int SearchMaxInRowRecursive(int* row, const int N, int colNo, int maxIndex) {
+    if (row[colNo] > row[maxIndex])
+        maxIndex = colNo;
+    if (colNo < N - 1)
+        return SearchMaxInRowRecursive(row, N, colNo + 1, maxIndex);
+    else
+        return maxIndex;
+}
+int SearchMinInRowRecursive(int* row, const int N, int colNo, int minIndex) {
+    if (row[colNo] < row[minIndex])
+        minIndex = colNo;
+    if (colNo < N - 1)
+        return SearchMinInRowRecursive(row, N, colNo + 1, minIndex);
+    else
+        return minIndex;
+}
+void SwapMaxMinRows(int** a, const int K, const int N, int rowNo) {
+    int maxIndex = SearchMaxInRowRecursive(a[rowNo], N, 0, 0);
+    int minIndex = SearchMinInRowRecursive(a[rowNo - 1], N, 0, 0);
+    int temp = a[rowNo][maxIndex];
+    a[rowNo][maxIndex] = a[rowNo - 1][minIndex];
+    a[rowNo - 1][minIndex] = temp;
+    cout << "Swapped elements from row " << rowNo << " and row " << rowNo - 1 << endl << endl;
+    if (rowNo < K - 1)
+        SwapMaxMinRows(a, K, N, rowNo + 2);
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab7.2(rec).cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		TEST_METHOD(Test_SearchMaxInRowRecursive)
+		{
+			int row[] = { 1, 5, 3, 9, 2 };
+			int maxIndex = SearchMaxInRowRecursive(row, 5, 0, 0);
+			Assert::AreEqual(3, maxIndex);
+		}
+		TEST_METHOD(Test_SearchMinInRowRecursive)
+		{
+			int row[] = { 1, 5, 3, 9, 2 };
+			int minIndex = SearchMinInRowRecursive(row, 5, 0, 0);
+			Assert::AreEqual(0, minIndex);
+		}
+		TEST_METHOD(Test_SwapMaxMinRows)
+		{
+			const int k = 2, n = 5;
+			int** a = new int* [k];
+			for (int i = 0; i < k; i++)
+				a[i] = new int[n];
+			int row1[] = { 1, 5, 3, 9, 2 };
+			int row2[] = { 6, 8, 7, 4, 10 };
+			for (int i = 0; i < n; i++) {
+				a[0][i] = row1[i];
+				a[1][i] = row2[i];
+			}
+			SwapMaxMinRows(a, k, n, 1);
+			Assert::AreEqual(10, a[0][0]);
+			Assert::AreEqual(1, a[1][4]);
+			for (int i = 0; i < k; i++)
+				delete[] a[i];
+			delete[] a;
+		}
+	};
+}
